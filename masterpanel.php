ece393f1f510c366f364bb3dfd650b365f3e9922
SzymiCzaykow <s_czaykowski@wp.pl>
2020-11-19 14:26:45 +0100

    Rozwój masterpanel.php
    Tworzenie masterpanel, wyzwanie: załatwić dynamiczne odbieranie zmiennych w php

--- masterpanel.php
+++ masterpanel.php
@@ -2,26 +2,119 @@
 <head>
 	<meta charset='utf=8' />
 	<title>Panel kierownika</title>
+	
+	<style>
+		#container
+		{
+			width: 1030;
+		}
+			
+		#naglowek
+		{
+			border: solid black 2px;
+			background-color: white;
+			padding: 3px;
+		}
+			
+		#content
+		{
+			border: solid black 2px;
+			background-color: white;
+			padding: 10px;
+		}
+		
+		#requestslist
+		{
+			width: 382;
+		}
+		
+		#buttons
+		{
+			width: 90;
+		}
+		
+		
+	</style>
+	
 </head>
-<body>
+<body bgcolor="teal">
 
-<form action='masterpanel.php' method='post'>
-Ile zmiennych dostaliśmy (roboczo)<br />
-<input type='text' name='zmienna1' /><br />
-<input type='submit' value='Zbuduj listę z tyloma elementami' /><br>
-</form>
+<div id="container">
 
-<?php
-error_reporting(0);
-$zmienna1 = $_POST['zmienna1'];
+	<div id="naglowek">
+		<h1>Karty wymagające podpisu kierownika działu: (tu zmienna z nazwą działu)</h1>
+	</div>
+	
+	<div style="padding: 5px;"></div>
+	
+	<div id="content">
+		<?php
+			error_reporting(0);
+			$zmienna1 = 5; //TUTAJ POWINNA TRAFIĆ LICZBA OKRESLAJĄCA LICZBĘ REKORDÓW, JAKIE CZEKAJĄ NA ZATWIERDZENIE
+			
+			echo "<font size=5>";
+			echo "<b>";
+			echo "	Pozycje do rozstrzygnięcia ($zmienna1):";
+			echo "</b>";
+			echo "</font>";
+			echo "</br>";
+			echo "</br>";
+			
+			//PRZYKŁADOWE ZMIENNE, W REALU BĘDĄ TO
+			//RZECZYWISTE, WYCIĄGNIĘTE Z TABELI DANE
+			
+			$IMIE = "Jan";
+			$NAZWISKO = "Kowalski";
+			$NR_PRACOWNIKA = "19";
+			$DATA_ZWOLNIENIA = "01/01/2020";
+			
+			echo "<div id='requestslist'>";
+				
+				echo "<div id='up'>";
+					echo "<form method='post' action='masterpanel.php'>";
+						for($i=0; $i<$zmienna1; $i++)
+						{
+							//TUTAJ INSTRUKCJA WYŚWIETLANIA POJEDYNCZEJ POZYCJI,
+							//IMIĘ, NAZWISKO, NUMER I DATA ZWOLNIENIA PRACOWNIKA
+							//(ODPOWIEDNIE DANE ZWYCZAJNIE SELECTEM Z TABELI WORKERS)
+							echo "<fieldset style='width: 350'>";
+							echo "<legend>$NAZWISKO $IMIE</legend>";
+								echo "Numer: $NR_PRACOWNIKA, zwolniony(a): $DATA_ZWOLNIENIA";
+								
+								echo "<div style='float: right'>";
+								echo "<input type='checkbox' name='czekbox'".($i+1)."' />";
+								echo "</div>";
+								
+							echo "</fieldset>";
+							echo "<br />";
+						}
+				echo "</div>";
+				
+				
+				echo "<div id='down' align='right'>";
+					echo "<div id='buttons'>";
+						echo "<input type='submit' name='accept' value='Zaakceptuj' style='display: block; width: 100%	' />";
+						echo "<input type='submit' name='decline' value='Odrzuć' style='display: block; width: 100%	'/>";
+					echo "</div>";
+				echo "</div>";
+				
+				
+			echo "</div>";
 
-echo "Lista pozycji do rozliczenia:<br />";
-for($i=0; $i<$zmienna1; $i++)
-{
-	echo "[".($i+1)."] Karta pracownika X<br />";
-}
+			
+			echo "</form>";
+			
+		?>
+		
+	</div>
+	
+	
+	
+	
+</div>
 
-?>
+
+
 
 </body>
 </html>
