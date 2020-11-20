3fa674e609040efefb3812cc0ced0b433263fc1e
SzymiCzaykow <s_czaykowski@wp.pl>
2020-11-20 10:16:39 +0100

    Usunięcie przycisku, zmienna=4

--- masterpanel.php
+++ masterpanel.php
@@ -1,47 +1,41 @@
-<?php
-include('db/config.php');
-session_start();
-
-?>
-
 <html>
 <head>
 	<meta charset='utf=8' />
 	<title>Panel kierownika</title>
-
+	
 	<style>
 		#container
 		{
 			width: 1030;
 		}
-
+			
 		#naglowek
 		{
 			border: solid black 2px;
 			background-color: white;
 			padding: 3px;
 		}
-
+			
 		#content
 		{
 			border: solid black 2px;
 			background-color: white;
 			padding: 10px;
 		}
-
+		
 		#requestslist
 		{
 			width: 382;
 		}
-
+		
 		#buttons
 		{
 			width: 90;
 		}
-
-
+		
+		
 	</style>
-
+	
 </head>
 <body bgcolor="teal">
 
@@ -50,15 +44,15 @@ session_start();
 	<div id="naglowek">
 		<h1>Karty wymagające podpisu kierownika działu: (tu zmienna z nazwą działu)</h1>
 	</div>
-
+	
 	<div style="padding: 5px;"></div>
-
+	
 	<div id="content">
 		<?php
 			error_reporting(0);
-
-			$ile = 5; //TUTAJ POWINNA TRAFIĆ LICZBA OKRESLAJĄCA LICZBĘ REKORDÓW, JAKIE CZEKAJĄ NA ZATWIERDZENIE
-
+			
+			$ile = 4; //TUTAJ POWINNA TRAFIĆ LICZBA OKRESLAJĄCA LICZBĘ REKORDÓW, JAKIE CZEKAJĄ NA ZATWIERDZENIE
+			
 			echo "<font size=5>";
 				echo "<b>";
 					echo "	Pozycje do rozstrzygnięcia ($ile):";
@@ -66,7 +60,7 @@ session_start();
 			echo "</font>";
 			echo "</br>";
 			echo "</br>";
-
+			
 			//PRZYKŁADOWE ZMIENNE, W REALU BĘDĄ TO
 			//RZECZYWISTE, WYCIĄGNIĘTE Z TABELI DANE
 			////////////////////////////////////
@@ -75,10 +69,10 @@ session_start();
 			$NR_PRACOWNIKA = "19";            //
 			$DATA_ZWOLNIENIA = "01/01/2020";  //
 			////////////////////////////////////
-
-
+			
+			
 			echo "<div id='requestslist'>";
-
+				
 				echo "<div id='up'>";
 					echo "<form method='post' action='masterpanel.php'>";
 						for($i=0; $i<$ile; $i++)
@@ -89,39 +83,38 @@ session_start();
 							echo "<fieldset style='width: 350'>";
 							echo "<legend>$NAZWISKO $IMIE</legend>";
 								echo "Numer: $NR_PRACOWNIKA, zwolniony(a): $DATA_ZWOLNIENIA";
-
+								
 								echo "<div style='float: right'>";
 								echo "<input type='checkbox' name='czekbox".($i+1)."' />";
 								echo "</div>";
-
+								
 							echo "</fieldset>";
 							echo "<br />";
 							/////////////////////////////////////////////////////////
 						}
 				echo "</div>";
-
-
+				
+				
 				echo "<div id='down' align='right'>";
 					echo "<div id='buttons'>";
 						echo "<input type='submit' name='accept' value='Zaakceptuj' style='display: block; width: 100%	' />";
-						echo "<input type='submit' name='decline' value='Odrzuć' style='display: block; width: 100%	'/>";
 					echo "</div>";
 				echo "</div>";
-
-
+				
+				
 			echo "</div>";
 
-
+			
 					echo "</form>";
-
-
+					
+					
 					//TUTAJ TABLICA ZMIENNYCH, DO KTÓRYCH BĘDĄ TRAFIAŁY WARTOŚCI
 					$checkboxtab[$ile];
-
+					
 					//TRZEBA JE JESZCZE WYPEŁNIĆ:
 					$prefix = "czekbox";
 					$nazwa;
-
+					
 					for($i=0; $i<$ile; $i++)
 					{
 						$nazwa = $prefix.(string)($i+1);
@@ -136,12 +129,12 @@ session_start();
 						else echo "Checkbox nr ".($i+1).": nie zaznaczono<br />";
 					}
 		?>
-
+		
 	</div>
-
-
-
-
+	
+	
+	
+	
 </div>
 
 
